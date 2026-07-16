498e4b329c174e32025f1a309a70e2a082083291
gagonzalezl <gagonzalezl@unal.edu.co>
2026-07-16 11:16:08 -0400

    Avoid RAM overfilling(windows)

--- src/research_ct/visualization/__init__.py
+++ src/research_ct/visualization/__init__.py
@@ -3,9 +3,6 @@
 from .napari_viewer import launch_napari_viewer
 from .plot_distributions import plot_gmm_components
 from .export import export_probability_video
-from .histogram_diagnostics_viewer import (
-    Plot_Histogram_Diagnostics,
-    Plot_Slice_Histograms
-)
+from .histogram_diagnostics_viewer import Plot_Histogram_Comparison, Plot_Slice_Histograms
 
-__all__ = ["launch_napari_viewer", "plot_gmm_components", "export_probability_video","Plot_Histogram_Diagnostics","Plot_Slice_Histograms"]
+__all__ = ["launch_napari_viewer", "plot_gmm_components", "export_probability_video","Plot_Histogram_Comparison","Plot_Slice_Histograms"]
